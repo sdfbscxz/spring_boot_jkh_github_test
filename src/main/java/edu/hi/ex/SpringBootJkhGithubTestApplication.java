be4ac8767ed2815d2405a88dd2421e40c1ed3956
sdfbscxz <ckh4880@gmail.com>
2022-07-11 10:06:22 +0900

    first commit

---
+++ src/main/java/edu/hi/ex/SpringBootJkhGithubTestApplication.java
@@ -0,0 +1,13 @@
+package edu.hi.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJkhGithubTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJkhGithubTestApplication.class, args);
+	}
+
+}
